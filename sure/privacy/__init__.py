3003c90635cd0d061f70de8a45c182bcfb7154cd
dariobrunelli <dariobrunelli95@gmail.com>
2024-07-22 17:32:57 +0200

    minor fix

--- sure/privacy/__init__.py
+++ sure/privacy/__init__.py
@@ -1,5 +1,6 @@
 from ..distance_metrics.distance import (distance_to_closest_record, 
                                          dcr_stats, 
+                                         number_of_dcr_equal_to_zero,
                                          dcr_histogram, 
                                          validation_dcr_test)
 from .privacy import (adversary_dataset, 
