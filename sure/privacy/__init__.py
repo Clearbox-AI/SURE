df1d3606d791fed36ce5581c99b807f41443d5d9
dariobrunelli <dariobrunelli95@gmail.com>
2024-07-22 15:51:41 +0200

    import fix

--- sure/privacy/__init__.py
+++ sure/privacy/__init__.py
@@ -3,8 +3,8 @@ from ..distance_metrics.distance import (distance_to_closest_record,
                                          number_of_dcr_equal_to_zero, 
                                          dcr_histogram, 
                                          validation_dcr_test)
-# from privacy import (adversary_dataset, 
-#                       membership_inference_test)
+from .privacy import (adversary_dataset, 
+                      membership_inference_test)
 
 __all__ = [
     "distance_to_closest_record",
@@ -12,6 +12,6 @@ __all__ = [
     "number_of_dcr_equal_to_zero",
     "dcr_histogram",
     "validation_dcr_test",
-    # "adversary_dataset",
-    # "membership_inference_test",
+    "adversary_dataset",
+    "membership_inference_test",
 ]
