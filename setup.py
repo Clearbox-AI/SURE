f7c70ee6f50826083cb03a94b3a2813c877f39bb
dariobrunelli <dariobrunelli95@gmail.com>
2024-08-01 19:08:54 +0200

    version 0.1.6

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ class CustomBuild(build_ext):
 setup(
     # Metadata about the package
     name="clearbox-sure",
-    version="0.1.5",
+    version="0.1.6",
     author="Clearbox AI",
     author_email="info@clearbox.ai",
     description="",
