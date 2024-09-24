16b634a68b2c9d7003a4152d5efc382c572e139d
dariobrunelli <dariobrunelli95@gmail.com>
2024-09-24 10:44:19 +0200

    added info label to ML utility in the report

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ class CustomBuild(build_ext):
 setup(
     # Metadata about the package
     name="clearbox-sure",
-    version="0.1.9.6",
+    version="0.1.9.7",
     author="Clearbox AI",
     author_email="info@clearbox.ai",
     description="",
