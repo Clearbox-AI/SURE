39a292e501c8c710b0e090dbe3bef88bfec54a00
dariobrunelli <dariobrunelli95@gmail.com>
2025-05-14 10:09:47 +0200

    fix in publish to pypi

--- build_wheel.sh
+++ build_wheel.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 set -e
-
 cd /io
 
 ${PYTHON_PATH}/pip install -U pip setuptools wheel Cython
 ${PYTHON_PATH}/pip install -r requirements.txt
 ${PYTHON_PATH}/python setup.py bdist_wheel
 
+# Prepare output directory
+mkdir -p /io/wheelhouse
+
 # Repair the wheels with auditwheel
 for whl in dist/*.whl; do
     auditwheel repair "$whl" --plat manylinux2014_x86_64 -w /io/wheelhouse/
+done
 
+# Replace dist with repaired wheels
 rm -rf dist
 mv wheelhouse dist
-done
